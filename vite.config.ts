9d5fc1c030d1d596deca86b66527a4dde3439990
Steve Savstrom <steve.savstrom@gmail.com>
2025-02-15 08:11:12 -0600

    Fix github actions

--- vite.config.ts
+++ vite.config.ts
@@ -5,14 +5,14 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === "development" ? "/" : "/simple-directory-explorer/", // Ensure base path is only set in production
+  base: mode === "production" ? "/simple-directory-explorer/" : "/", // Base path for GitHub Pages
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
     react(),
-    mode === 'development' && componentTagger(),
+    mode === "development" && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
