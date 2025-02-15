1b6a4ac706209c0ecde1d92cf315a999c2a884df
Steve Savstrom <steve.savstrom@gmail.com>
2025-02-15 08:02:08 -0600

    Update base path

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/",
+  base: "/simple-directory-explorer/",
   server: {
     host: "::",
     port: 8080,
